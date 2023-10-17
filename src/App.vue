bc660133ab3851e27a624c0428a4725695dea750
ZHAO Jin-Xiang <xiaoxiangmoe@gmail.com>
2023-10-17 13:39:41 +0800

    chore: add HelloWorld component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
-<script setup lang="ts"> 
+<script setup lang="ts">
+import HelloWorld from './HelloWorld.vue';
+
+const handleClick = (event: MouseEvent) => {
+  console.log(event);
+};
+ 
 </script>
 
 <template>
   <div id="app">
+    <HelloWorld @click="handleClick" />
   </div>
 </template>
---
+++ src/HelloWorld.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+interface ButtonProps {
+    border?: boolean; 
+}
+
+const props = defineProps<ButtonProps>() 
+defineEmits<{
+    (event: 'click',payload: MouseEvent): void
+}>()
+</script>
+<template>
+    <div id="app">
+        <button @click="$emit('click', $event)">Click me</button>
+    </div>
+</template>
